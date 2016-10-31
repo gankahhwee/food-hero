5242fd84a7fba258f7f0627f1d9950cc4f4af637
Ryan Phung <ryan.phung@gmail.com>
2016-11-01 02:21:55 +0800

    Do not recreate map when refresh

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -54,6 +54,9 @@ angular.module('starter.controllers', [])
   }
   
   var createMap = function(center){
+    if (typeof($scope.map) != 'undefined' && $scope.map != null)
+        return;
+
     var mapOptions = {
       center: center,
       zoom: 16,
